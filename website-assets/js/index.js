9a82ce37551f0d7ff47df50a0b1179ffc435999d
Godje <comepot.4anell@gmail.com>
2018-08-23 01:05:01 -0700

    Warning message. From now on the changes will be moved to branches and pushed to master when tested and completed

--- website-assets/js/index.js
+++ website-assets/js/index.js
@@ -5,6 +5,7 @@ const App = require("./modules/App.js");
 
 const root = document.querySelector(".root");
 
+alert("Warning! This website is currently under development. It is not functional, or functioning as should. This alert will no longer be shown when a functioning, advertiser-worthy release will be pushed.")
 m.route(root, "/", {
 	"/": App
 })
