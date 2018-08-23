e831c6c2feb2b9c2b66a71a4cb5a6044d9985812
Godje <comepot.4anell@gmail.com>
2018-08-22 20:24:59 -0700

    Merge

--- website-assets/js/modules/Monitor.js
+++ website-assets/js/modules/Monitor.js
@@ -1,8 +1,10 @@
 const m = require("mithril");
 m.stream = require("mithril-stream");
-const Tiles = require("./Tiles.js")
+const Tiles = require("./Tiles.js");
+const Modal = require("./Modal.js");
 
 const canvasSize = 600;
+const modalOpen = true;
 
 const Canvas = {
 	view: function (vnode){
@@ -17,7 +19,6 @@ class Monitor {
 	constructor(vnode){
 		this.ctrl = {
 			createTile: function (e){
-				console.log(e)
 				return;
 			},
 			saveSession: function (e){
@@ -38,12 +39,17 @@ class Monitor {
 				m("button", {
 					class: "add-tile",
 					onclick: this.ctrl.createTile
-				}, "Add Tile"),
+				}, "+ Add Tile"),
 				m("button", {
 					class: "save-session",
 					onclick: this.ctrl.saveSession
 				}, "Save JSON session")
-			])
+			]),
+			modalOpen ? m(Modal, [
+				m("div.wrapper", [
+					"Test"
+				]),
+			]) : "",
 		]);
 	}
 }
